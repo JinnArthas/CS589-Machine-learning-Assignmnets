750a1b1904fe99617f41888a1315b116b449567b
Ravi Agrawal <raviagrawal@Ravis-MacBook-Pro.local>
2017-11-22 01:03:44 -0500

    Everything

--- HW04/Submission/Code/run_me.py
+++ HW04/Submission/Code/run_me.py
@@ -3,8 +3,10 @@ import numpy as np
 import kaggle
 import random
 from random import randrange
+import matplotlib.pyplot as plt
 
 def p_j_alpha(j, alpha):
+    #A
     # Function to calculate the probability pr(J|alpha) 
     # takes input j (list) and alpha (int)
     # returns the probability (float)
@@ -18,6 +20,7 @@ def p_j_alpha(j, alpha):
 
 
 def p_b_j(b, j, dic):
+    #B
     # Function to calculate the probability pr(B|J) 
     # takes input b (list), J(list) and dic (dictionary of probability)
     # returns the X_i probability (float)
@@ -28,6 +31,7 @@ def p_b_j(b, j, dic):
 
 
 def p_alpha(alpha):
+    #C
     # Function to calculate alpha 
     # takes alpha (float ) as input 
     if alpha >= 0.0 and alpha <= 1.0:
@@ -37,12 +41,14 @@ def p_alpha(alpha):
     
     
 def p_alpha_j_b(b, j, alpha):
+    # D
     # Function to calculate pr(alpha,j,B)
     # takes alpha(int), j(list) and B(list)
     # returns pr(alpha,j,B) (float) 
     return p_alpha(alpha)*p_b_j(b, j, dic)*p_j_alpha(j, alpha)
 
 def random_j(j):
+    # E
     # Function to flip certain index of j
     length  = len(j)
     index = np.random.randint(0, length)
@@ -50,7 +56,8 @@ def random_j(j):
     j_new[index] = int(not j_new[index])
     return j_new
 
-def p_j_alpha_b(b, alpha, iteration):
+def p_j_alpha_b(b, alpha, iteration, Name, graph = False):
+    # F
     # Implementation of Metropolis Hastings algorithm 
     #to draw samples from P (J|α, B) and then 
     # calculate hen calculate the mean value of J in those samples.
@@ -63,15 +70,27 @@ def p_j_alpha_b(b, alpha, iteration):
         if random.random() <= acceprance_ratio:
             j = j_new
         j_mean += j
-    return j_mean/(1.0*iteration)
+    result = j_mean/(1.0*iteration)
+    if graph == True:
+        index = np.arange(len(result))
+        plt.bar(index, result)
+        plt.ylabel("P(J|α, B)")
+        plt.xlabel("J mean")
+        plt.title("Test case for: " + str(b))
+        plt.ylim( 0.0, 1.0 )  
+        plt.savefig("../Figures/ques_F_test_" + str(Name)+ ".png")
+        plt.close()
+    return result
  
 def random_alpha(alpha):
+    # g
     # Function to randomly calculate random alpha
     # input alpha (flaot)
     # return random alpha value between 0.0 and 1.0
     return np.random.rand()
 
-def p_alpha_given_j_b(j, b, iteration):
+def p_alpha_given_j_b(j, b, iteration, graph = False,  Name = 1):
+    # h
     #function for Metropolis Hastings algorithm 
     #to draw samples from P (α|J, B) and
     #then calculate the mean value of α in those samples
@@ -86,22 +105,33 @@ def p_alpha_given_j_b(j, b, iteration):
         if random.random() <= acceprance_ratio:
             alpha = alpha_new
         alpha_mean.append(alpha)
+    if graph == True:
+        #index = np.arange(len(alpha_mean))
+        plt.hist(alpha_mean, bins = 50)
+        plt.ylabel("P(α|J, B)")
+        plt.xlabel("Alpha at perticular bins")
+        plt.title("Test case for: " + str(b))
+        #plt.ylim( 0.0, 1.0 )  
+        plt.savefig("../Figures/ques_h_test_" + str(Name)+ ".png")
+        plt.close()
     return sum(alpha_mean)/(1.0*iteration)
 
 def proposal_alpha_j(j, alpha):
+    # i
     # returns proposal alpha and j 
     # input j (list) and alpha (float)
     # return flipped j (list) and random alpha (alpha)
     return random_j(j), random_alpha(alpha)
     
 
-def p_new_alpha_new_j_b(b, iteration):
+def p_new_alpha_new_j_b(b, iteration, graph = False, Name = 1):
+    # j
     #function for Metropolis Hastings algorithm
     #to draw samples from P (α, J|B) and
     #then calculate the mean values of α and J in those Samples.
     # Input b (list) and iteration (int)
     # outputs P (J|α, B), P (α|J, B)
-    alpha_mean = 0.0
+    alpha_mean = []
     alpha = random.random()
     j = np.array([0 for i in range(len(b))])
     j_mean = np.array([0 for i in range(len(b))])
@@ -112,9 +142,39 @@ def p_new_alpha_new_j_b(b, iteration):
         if random.random() <= acceprance_ratio:
             alpha = alpha_new
             j = j_new
-        alpha_mean += alpha
+        alpha_mean.append(alpha)
         j_mean += j
-    return j_mean/(1.0*iteration), alpha_mean/(1.0*iteration)
+        
+    if graph == True:
+        index = np.arange(len(j_mean/(1.0*iteration)))
+        plt.bar(index, j_mean/(1.0*iteration))
+        plt.ylabel("P(J|B)")
+        plt.xlabel("J mean")
+        plt.title("Test case for: " + str(b))
+        plt.ylim( 0.0, 1.0 )  
+        plt.savefig("../Figures/ques_j_1_test_" + str(Name)+ ".png")
+        plt.close()
+        
+
+        index = np.arange(len(alpha_mean))
+        plt.bar(index, alpha_mean)
+        plt.ylabel("P(α|B)")
+        plt.xlabel("Alpha over 10000 iterations")
+        plt.title("Test case for: " + str(b))
+        plt.ylim( 0.0, 1.0 )  
+        plt.savefig("../Figures/ques_j_3_test_" + str(Name)+ ".png")
+        plt.close()
+    
+        #index = np.arange(len(alpha_mean))
+        plt.hist(alpha_mean, bins = 50)
+        plt.ylabel("P(α|J, B)")
+        plt.xlabel("Alpha at perticular bins")
+        plt.title("Test case for: " + str(b))
+        #plt.ylim( 0.0, 1.0 )  
+        plt.savefig("../Figures/ques_j_2_test_" + str(Name)+ ".png")
+        plt.close()
+        
+    return j_mean/(1.0*iteration), sum(alpha_mean)/(1.0*iteration)
 
 def p_n_1_given_alpha(j_n, alpha):
     # returns input P (B n+1 |J n , α)
@@ -167,24 +227,25 @@ if __name__ == '__main__':
     print("\n*************************Solution for question 1 f *******************************")
     #dic = {(0 ,0) : 0.20, (0, 1): 0.90, (1, 0): 0.80, (1,1) : .10}
     iteration = 10000
-    test_case = [[[1,0,0,1,1], 0.5], [[1,0,0,0,1,0,1,1], 0.11], [[1,0,0,1,1,0,0], 0.75]]
+    test_case = [[[1,0,0,1,1], 0.5, True], [[1,0,0,0,1,0,1,1], 0.11, False], [[1,0,0,1,1,0,0], 0.75, False]]
     for i,test in enumerate(test_case):
-        b, alpha = test[0],test[1] 
-        print("p_j_alpha_b for test case " + str(i + 1) +  ": " + str(p_j_alpha_b(b, alpha, iteration)))
+        b, alpha, graph = test[0],test[1], test[2] 
+        print("p_j_alpha_b for test case " + str(i + 1) +  ": " + str(p_j_alpha_b(b, alpha, iteration, i, graph)))
         
     print("\n*************************Solution for question 1 h *******************************")
     #dic = {(0 ,0) : 0.20, (0, 1): 0.90, (1, 0): 0.80, (1,1) : .10}
-    test_cases = [[[0,1,0,1,0], [1,0,1,0,1]], [[0,0,0,0,0], [1,1,1,1,1]],
-                  [[0,1,1,1,1,1,1,0], [1,0,0,1,1,0,0,1]], 
-                  [[0,1,1,0,1,0], [1,0,0,1,1,1]]]
+    test_cases = [[[0,1,0,1,0], [1,0,1,0,1], False], [[0,0,0,0,0], [1,1,1,1,1], False],
+                  [[0,1,1,1,1,1,1,0], [1,0,0,1,1,0,0,1], False],
+                  [[0,1,1,0,1], [1,0,0,1,1], True],
+                  [[0,1,1,0,1,0], [1,0,0,1,1,1], False]]
     iteration = 10000.0
     for i, test_case in enumerate(test_cases):
-        j, b = test_case[0], test_case[1]
-        print("p_alpha_given_j_b for test case " + str(i + 1) +  ": " + str(p_alpha_given_j_b(j, b, iteration)))
+        j, b, graph = test_case[0], test_case[1], test_case[2]
+        print("p_alpha_given_j_b for test case " + str(i + 1) +  ": " + str(p_alpha_given_j_b(j, b, iteration, graph)))
         
     print("\n*************************Solution for question 1 j *******************************")
     b = np.array([1,1,0,1,1,0,0,0])
-    print("p_new_alpha_new_j_b: " + str(p_new_alpha_new_j_b(b, iteration)))
+    print("p_new_alpha_new_j_b: " + str(p_new_alpha_new_j_b(b, iteration, True)))
     
     print("\n*************************Solution for question 1 k *******************************")
     test_cases = [[0.33456, 0.0], [0.5019, 1.0], [0.33456, 0.0], [0.5019, 1.0]]
